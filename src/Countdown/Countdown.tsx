f9a84b4d7a939b229dee918b3ee3581a7ba6c137
Wack Overflow cryptobilly37@gmail.com <cryptobilly37@gmail.com>
2021-05-11 08:30:01 -0600

    Countdown component

---
+++ src/Countdown/Countdown.tsx
@@ -0,0 +1,67 @@
+import { Box, makeStyles, Typography } from '@material-ui/core';
+import clsx from 'clsx';
+import CountdownBase from 'react-countdown'
+
+import BuyButton from '../BuyButton'
+import countdownData from '../_countdownData.json'
+
+const useStyles = makeStyles(({ spacing }) => ({
+  overlay: {
+    background: 'rgba(0, 0, 0, .2)',
+    borderRadius: 20,
+    height: 'auto',
+    width: '50%',
+    minWidth: 400,
+    color: '#fff',
+    textShadow: '4px 4px 2px rgba(0,0,0,0.2)',
+    padding: spacing(3),
+  },
+  countdownItem: {
+    background: 'rgba(0, 0, 0, .2)',
+    margin: spacing(1),
+    borderRadius: 5,
+    padding: spacing(1),
+  },
+}))
+
+const Countdown: React.FC<{ className?: string }> = ({ className }) => {
+  const classes = useStyles();
+
+  return (
+    <Box
+      className={clsx(className, classes.overlay)}
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+    >
+      <Typography variant="h4">{countdownData.title}</Typography>
+      <CountdownBase
+        date={new Date(countdownData.date)}
+        renderer={({ days, hours, minutes, seconds, completed }) => {
+          if (completed) {
+            return <BuyButton />
+          } else {
+            return (
+              <Box display="flex">
+                <Typography variant="h5" component="span" className={classes.countdownItem}>
+                  {days}d
+              </Typography>
+                <Typography variant="h5" component="span" className={classes.countdownItem}>
+                  {hours}h
+              </Typography>
+                <Typography variant="h5" component="span" className={classes.countdownItem}>
+                  {minutes}m
+              </Typography>
+                <Typography variant="h5" component="span" className={classes.countdownItem}>
+                  {seconds}s
+              </Typography>
+              </Box>
+            )
+          }
+        }}
+      />
+    </Box>
+  )
+}
+
+export default Countdown;
--- src/SectionHero/SectionHero.tsx
+++ src/SectionHero/SectionHero.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import { Box, Container, makeStyles, Typography } from '@material-ui/core'
-import Countdown from 'react-countdown'
-import countdownData from '../_countdownData.json'
-import BuyButton from '../BuyButton'
+import { Container, makeStyles } from '@material-ui/core'
 import heroImage from './seal.png'
+import Countdown from '../Countdown';
 
 const useStyles = makeStyles(({ spacing, breakpoints }) => ({
   root: {
@@ -21,27 +19,14 @@ const useStyles = makeStyles(({ spacing, breakpoints }) => ({
       margin: `${spacing(6)}px auto`,
     },
   },
-  overlay: {
+  countdown: {
+
     position: 'absolute',
     top: 200,
     left: 0,
     right: 0,
     margin: '0 auto',
-    background: 'rgba(0, 0, 0, .2)',
-    borderRadius: 20,
-    height: 'auto',
-    width: '50%',
-    minWidth: 400,
-    color: '#fff',
-    textShadow: '4px 4px 2px rgba(0,0,0,0.2)',
-    padding: spacing(3),
-  },
-  countdownItem: {
-    background: 'rgba(0, 0, 0, .2)',
-    margin: spacing(1),
-    borderRadius: 5,
-    padding: spacing(1),
-  },
+  }
 }))
 
 const SectionHero: React.FC = () => {
@@ -49,39 +34,7 @@ const SectionHero: React.FC = () => {
   return (
     <Container id="hero" className={classes.root}>
       <img src={heroImage} alt="" className={classes.heroImage} />
-      <Box
-        className={classes.overlay}
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-      >
-        <Typography variant="h4">{countdownData.title}</Typography>
-        <Countdown
-          date={new Date(countdownData.date)}
-          renderer={({ days, hours, minutes, seconds, completed }) => {
-            if (completed) {
-              return <BuyButton />
-            } else {
-              return (
-                <Box display="flex">
-                  <Typography className={classes.countdownItem}>
-                    {days}d
-                  </Typography>
-                  <Typography className={classes.countdownItem}>
-                    {hours}h
-                  </Typography>
-                  <Typography className={classes.countdownItem}>
-                    {minutes}m
-                  </Typography>
-                  <Typography className={classes.countdownItem}>
-                    {seconds}s
-                  </Typography>
-                </Box>
-              )
-            }
-          }}
-        />
-      </Box>
+      <Countdown className={classes.countdown} />
     </Container>
   )
 }
