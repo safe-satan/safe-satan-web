36b978ced402d55fba07d522255ff94e890d2246
Wack Overflow cryptobilly37@gmail.com <cryptobilly37@gmail.com>
2021-08-10 15:24:38 -0600

    Tweak app script

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,3 @@
-const path = require("path");
 const express = require("express");
 const app = express(); // create express app
 const port = process.env.PORT ?? 3000;
@@ -7,5 +6,5 @@ app.use('/', express.static('build'))
 
 // start express server on port 5000
 app.listen(port, () => {
-  console.log(`server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
